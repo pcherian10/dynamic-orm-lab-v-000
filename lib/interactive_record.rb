f56dd1c293ed84eef433ef1d691c37a455fd867c
Paul <pcherian10@gmail.com>
2018-06-10 21:23:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,13 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  class Student
+    def self.table_name
+      self.to_s.downcase.pluralize
+    end
+  end
+
+
+   
 end
